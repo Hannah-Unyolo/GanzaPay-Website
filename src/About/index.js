3bab225eab9c0841d25dfe6bd9e83dbfe90e762f
Cynthia-mueni <nthenyamuenicynthia@gmail.com>
2024-06-16 22:40:35 +0300

    added about

---
+++ src/About/index.js
@@ -0,0 +1,32 @@
+import './index.css';
+const Abouts = () => {
+  return (
+
+    <div className="about-text">
+      {/* <div class='about-us'>ABOUT US</div> */}
+      <h3>ABOUT US</h3>
+      <h2>Who are we?</h2> 
+      <p>
+        We are team of developers with innovative solutions to everyday<br></br>
+        problem.At GanzaPay we believe there is a better way to do money<br></br>
+        transaction. A more valuable way were we consider every users needs. We<br></br>
+        have user friendly website that enables uses to navigate the app without<br></br>
+        any problems. We strive to simplify the way mobile apps feel to our<br></br>
+        users by using visual ques
+      </p>
+      
+      <h2>Mission</h2>
+      <p>
+        Our Mission is to become the most reliable parther for promoting access
+        to mobile money for all </p>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+export default Abouts;
